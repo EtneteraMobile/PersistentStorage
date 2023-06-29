5d066e09ac3af727d6ff86ee73c32b8c27c389c0
Antonin Simek <antonin.simek@etnetera.cz>
2023-06-29 16:26:01 +0200

    1 Added documentation and privated persisted method

--- PersistentStorage/Sources/Data/Repository/PersistentStorage+Codable.swift
+++ PersistentStorage/Sources/Data/Repository/PersistentStorage+Codable.swift
@@ -18,6 +18,11 @@ extension UserDefaults {
 }
 
 public extension PersistentStorage {
+    /// Method for storing a codable value with assigned key into the user defaults storage.
+    ///
+    /// - Parameters:
+    ///     - value: The value you want to store in
+    ///     - key: Key that will be assigned to value
     @discardableResult
     func store<T: Codable>(
         _ value: T?,
@@ -44,6 +49,15 @@ public extension PersistentStorage {
             .eraseToAnyPublisher()
     }
 
+    /// Method for reading a codable value with given key and type. Result is returned as a publisher.
+    ///
+    /// - Possible failures:
+    ///     - If type resolution with given parameter `valueType` fails, method will return `noValueFoundWithGivenType` failure.
+    ///     - If no value is associated with key given in parameter `valueKey` or its value is nil, the method will return `noValueFound` failure.
+    ///
+    /// - Parameters:
+    ///     - valueType: The type of value that you wish to read.
+    ///     - valueKey: Key that is assigned to a value that you wish to read.
     func readWithPublisher<T: Codable>(
         valueType: T.Type,
         valueKey: String
@@ -68,7 +82,7 @@ public extension PersistentStorage {
         }
     }
 
-    func getCodablePersistedValue<T: Codable>(
+    private func getCodablePersistedValue<T: Codable>(
         valueType: T.Type,
         valueKey: String
     ) throws -> T {
